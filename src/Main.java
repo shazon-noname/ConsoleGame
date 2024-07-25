0c6fa9058ca2f63f7b4a866801e3c1180bd421a7
shazon <maryan.20083@gmail.com>
2024-07-25 10:41:02 +0300

    add displayHangman() method and refactor Scanner

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,44 @@ import java.util.Scanner;
 
 public class Main {
     static int countFail = 0;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String word = randomWords();
-        String result = word.replaceAll(".", "*");
-        System.out.println("Guess the word: " + result);
-
-        while (checkForUnknownLetters(result)) {
+        try {
+            String word = randomWords();
+            String result = word.replaceAll(".", "*");
+            System.out.println("Guess the word: " + result);
             System.out.println("You have five tries" + "\n\nEnter a character:");
-            if (countFail == 5) {
-                System.out.println("Game over! You've used all attempts.");
-                System.exit(1);
+
+            while (checkForUnknownLetters(result)) {
+                if (countFail == 5) {
+                    displayHangman();
+                    System.exit(1);
+                }
+
+                String consoleChar = readConsoleLetter();
+                String newResult = checkedWordInRound(word, result, consoleChar);
+
+                if (result.equals(newResult)) {
+                    countFail++;
+                    displayHangman();
+                    System.out.println("Incorrect guess. You have " + (5 - countFail) + " attempts left.");
+                } else {
+                    System.out.println("Good guess! Keep going.");
+                }
+
+                result = newResult;
+
+                System.out.println(result);
             }
-
-            String consoleChar = readConsoleLetter();
-            String newResult = checkedWordInRound(word, result, consoleChar);
-
-            if (result.equals(newResult)) {
-                countFail++;
-                System.out.println("Incorrect guess. You have " + (5 - countFail) + " attempts left.");
-            } else {
-                System.out.println("Good guess! Keep going.");
-            }
-
-            result = newResult;
-
-            System.out.println(result);
+            System.out.println("Congratulations! You've guessed the word: " + word);
+        } finally {
+            scanner.close();
         }
-        System.out.println("Congratulations! You've guessed the word: " + word);
     }
 
 
     public static String readConsoleLetter() {
-        Scanner scanner = new Scanner(System.in);
         String consoleLetter = scanner.nextLine();
         while (consoleLetter.length() != 1) {
             System.out.println("You typed a word or more than 1 letter. Please enter a single letter:");
@@ -72,4 +77,54 @@ public class Main {
         return stringBuilder.toString();
     }
 
+    public static void displayHangman() {
+        switch (countFail) {
+            case 1:
+                System.out.println("  _______  ");
+                System.out.println(" |/      | ");
+                System.out.println(" |      (_) ");
+                System.out.println(" |          ");
+                System.out.println(" |          ");
+                System.out.println(" |          ");
+                System.out.println("_|___       ");
+                break;
+            case 2:
+                System.out.println("  _______  ");
+                System.out.println(" |/      | ");
+                System.out.println(" |      (_) ");
+                System.out.println(" |       |  ");
+                System.out.println(" |       |  ");
+                System.out.println(" |          ");
+                System.out.println("_|___       ");
+                break;
+            case 3:
+                System.out.println("  _______  ");
+                System.out.println(" |/      | ");
+                System.out.println(" |      (_) ");
+                System.out.println(" |      \\|  ");
+                System.out.println(" |       |  ");
+                System.out.println(" |          ");
+                System.out.println("_|___       ");
+                break;
+            case 4:
+                System.out.println("  _______  ");
+                System.out.println(" |/      | ");
+                System.out.println(" |      (_) ");
+                System.out.println(" |      \\|/ ");
+                System.out.println(" |       |  ");
+                System.out.println(" |          ");
+                System.out.println("_|___       ");
+                break;
+            case 5:
+                System.out.println("  _______  ");
+                System.out.println(" |/      | ");
+                System.out.println(" |      (_) ");
+                System.out.println(" |      \\|/ ");
+                System.out.println(" |       |  ");
+                System.out.println(" |      / \\ ");
+                System.out.println("_|___       ");
+                System.out.println("Game over! You've used all attempts.");
+                break;
+        }
+    }
 }
